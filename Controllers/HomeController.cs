29110b0dae0b5c53dc42224b268af2c255849e2c
Maronda <marondaanderson@gmail.com>
2021-10-28 21:55:01 -0400

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using Assessment5a.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assessment5a.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+        public IActionResult Calculator()
+        {
+            return View(new Calc());
+        }
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Result()
+        {
+            return View();
+        }
+
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/Calc.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assessment5a.Models
+{
+    //Step 1. You have to create property in order to create form inputs
+    public class Calc
+    {
+
+        public int num1 { get; set;  }
+        public int num2 { get; set;  }
+        public string operation { get; set;  }
+    }
+}
